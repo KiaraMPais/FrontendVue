1def0baa09913f63e034382dd94653b517ba24ac
KiaraMPais <kikipais@hotmail.com>
2025-03-11 08:01:36 -0300

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <CFooter class="px-4">
+    <div>
+      <a>Coto C.I.C.S.A.</a>
+      <span class="ms-1"
+        >&copy; {{ new Date().getFullYear() }}</span
+      >
+    </div>
+  </CFooter>
+</template>
